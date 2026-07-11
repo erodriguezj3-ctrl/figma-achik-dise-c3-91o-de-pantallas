d69da3db7c3149cd92cb9f5b2b15cb693eba1301
EDWIN CARLOS RODRIGUEZ JUELA <erodriguezj3@unemi.edu.ec>
2026-07-11 06:36:59 +0000

    Manual code change

--- src/config/videos.js
+++ src/config/videos.js
@@ -15,17 +15,17 @@
 
 export const VIDEO_SOURCES = {
   // --- Videos de Introducción (menús de curso) ---
-  intro_basica: "https://drive.google.com/file/d/161-ld4iIenLHDJSOKOzmplK9r90IUDHR/view?usp=drive_link",           // ← Video Introductorio de Fotografía Básica
-  intro_intermedia: "https://drive.google.com/file/d/1YhIl_ZdNF-8PBz9bARisq57vrZWmHVof/view?usp=drive_link",       // ← Video Introductorio de Fotografía Intermedia
-  intro_avanzada: "",         // ← Subir más adelante (placeholder visible)
+  intro_basica: "https://drive.google.com/uc?export=download&id=161-1d4iIenLHDJSOKOzmplK9r90IUDHR",
+  intro_intermedia: "https://drive.google.com/uc?export=download&id=1YhIl_ZdNF-8PBz9bARisq57vrZWmHVof",
+  intro_avanzada: "",
 
   // --- Videos de Clases Específicas ---
-  clase_triangulo: "https://drive.google.com/file/d/1BQfU8a_EMlzC0nr_eHMWqUwgtee5YPfm/view?usp=drive_link",        // ← Clase: Triángulo de Exposición (.mp4)
-  clase_composicion: "https://drive.google.com/file/d/13NuVwfPkwczUMbNxRSosilNRUVDdlNyo/view?usp=drive_link",      // ← Clase: Composición / Regla de Tercios (.mp4)
-  clase_larga_exposicion: "", // Subir más adelante
-  clase_nocturna: "",         // Subir más adelante
-  clase_alta_velocidad: "",   // Subir más adelante
-  clase_profundidad: "",      // Subir más adelante
-  clase_bokeh: "",            // Subir más adelante
-  clase_luces: "",            // Subir más adelante
+  clase_triangulo: "https://drive.google.com/uc?export=download&id=1BQfU8a_EMlzC0nr_eHMWqUwgtee5YPfm",
+  clase_composicion: "https://drive.google.com/uc?export=download&id=13NuVwfPkwczUMbNxRosiINRUVddlNyo",
+  clase_larga_exposicion: "",
+  clase_nocturna: "",
+  clase_alta_velocidad: "",
+  clase_profundidad: "",
+  clase_bokeh: "",
+  clase_luces: ""
 };
